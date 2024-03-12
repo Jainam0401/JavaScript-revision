2dca8db3cebb5f7559351db9bf817b7129dc9d9d
Jainam Mithalal Jain <110166714+Jainam0401@users.noreply.github.com>
2024-03-12 11:53:12 +0000

    new chapters

---
+++ basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountName =  "constant keyword"
+let mail = "jainam@gmail.com"
+var pass = "yoyo honey sighn"
+username = "pokemon"
+
+pass = "changed password"
+mail = "new@mail"
+username = "pikachu"
+
+console.table([pass,mail,username,accountName])
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+
+
+let age = 23
+const name = "JAinam"
+let isCredit = true
+let state;
+let temperature = null
+
+console.log(typeof null) // object 
+console.log(typeof undefined); // undefined
